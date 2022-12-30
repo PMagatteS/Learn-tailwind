d3d8df05786a2c973e54ee00292df0fdb6c38f3e
PMagatteS <papamagattesady@gmail.com>
2022-12-30 22:36:31 +0000

    Testing my first classes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
-
+import Image from "next/image"
 
 export default function Home() {
   return (
    <>
-    <h1 className="text-3xl font-bold underline text-green-300">
-      Hello world!
-    </h1>
+ <div className="w-72 h-80 bg-slate-50 rounded-md drop-shadow-md m-auto">
+  <div className="w-16 h-16 relative rounded-full mx-auto my-2 ">
+              <Image
+              src="/images/profilepicture.jpg"
+              className="rounded-full object-cover"
+              fill={true}              
+              alt=""
+              />
+              </div>
+
+ </div>
    </>
   )
 }
